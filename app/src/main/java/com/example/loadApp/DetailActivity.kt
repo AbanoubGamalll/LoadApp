4e14de30dfbf2859c992c1e334b9ee80ed8b83b9
abanoubgamal33 <abanoubgamal33@gmail.com>
2022-09-12 14:28:43 +0200

    starter Code

---
+++ app/src/main/java/com/example/loadApp/DetailActivity.kt
@@ -0,0 +1,15 @@
+package com.example.loadApp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+
+        setSupportActionBar(findViewById(R.id.toolbarDetail))
+
+
+    }
+}
